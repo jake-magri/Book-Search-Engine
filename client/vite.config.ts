f4433b18f405c5cab23d394163a77cd3d8af2ca4
Bread <jmagri1@uncc.edu>
2024-11-20 20:57:23 -0500

    added apollo server in package.json files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,15 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+    server: {
     port: 3000,
     open: true,
     proxy: {
+      // TODO: Uncomment the following code once you have built the queries and mutations in the client folder
+      // '/graphql': {
+      // TODO: Comment out the following code once you have built the queries and mutations in the client folder
       '/api': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
+      },
     }
   }
 })
